25d682c82a36b501348a13c7c2e4d451f46e5b94
swamyvitasoft <swamy.vitasoft@gmail.com>
2024-02-08 12:12:59 +0530

    url root changed

--- backend/app/Config/App.php
+++ backend/app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'https://s2swebsolutions.com/karya/';
+    public string $baseURL = 'https://demos.s2swebsolutions.com/karya/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
